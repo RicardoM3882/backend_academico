09c24edd52915712172078d2e23d810a8bd3cf6e
RicardoM <ricardom3882@gmail.com>
2024-10-08 21:03:49 -0300

    atualização do todo update

--- aula3/projeto/urls.py
+++ aula3/projeto/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from todo.views import TodoListView, TodoCreateView, TodoUpdateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", TodoListView.as_view(), name="todo_list"),
+    path("create/", TodoCreateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
 ]
--- aula3/todo/views.py
+++ aula3/todo/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Todo
 
-# Create your views here.
+class TodoListView(ListView):
+    model = Todo
+    template_name = "todo/todo_list.html"
+    context_object_name = "todos"
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
+    model = Todo 
+    fields = ["title", "deadline"] 
+    success_url = reverse_lazy("todo_list")
